e853280fa31b65627222595210595541a7f2e99c
oussamakami <kamili.oussama1@gmail.com>
2020-12-02 04:33:03 +0100

    add date display

--- script.js
+++ script.js
@@ -19,8 +19,30 @@ const rotateHour = (date, minuteVal) => {
     hourHand.style.transform = `rotate(${(hourVal*30)+degFromMin}deg)`;
 }
 
+const weekDay = date => {
+    const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+          dateIndex = date.getDay();
+    return weekDays[dateIndex];
+}
 
-document.onload = setInterval(() => {
+const monthName = date => {
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+          monthIndex = date.getMonth();
+    return monthNames[monthIndex];
+}
+
+const displayDate = date => {
+    const dateOutput = document.getElementById("date-text"),
+          dayNumber = date.getDate();
+
+    dateOutput.innerHTML = `${weekDay(date)},<br/>${monthName(date)} ${dayNumber}`;
+}
+
+window.addEventListener("load", () => {
+    setInterval(() => {
+        let dd = new Date();
+        rotateMin(dd);
+    }, 100);
     let dd = new Date();
-    rotateMin(dd);
-}, 100);
+    displayDate(dd);
+})
